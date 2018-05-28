514e8f832e71e85ffbae8181b9cfe0b495bcc960
Henning Klokkeråsen <henning.klokkerasen@creuna.no>
2018-05-28 22:50:03 +0200

    Added integration tests for things to save

--- Clockwork.Vault.Integrations.Tidal.Tests/IntegrationTests/TidalIntegrationTests.cs
+++ Clockwork.Vault.Integrations.Tidal.Tests/IntegrationTests/TidalIntegrationTests.cs
@@ -1,21 +1,67 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using System.Threading.Tasks;
 using FluentAssertions;
 using NUnit.Framework;
+using OpenTidl.Methods;
 
 namespace Clockwork.Vault.Integrations.Tidal.Tests.IntegrationTests
 {
     [TestFixture]
     public class TidalIntegrationTests
     {
+        private OpenTidlSession _openTidlSession;
+        
         [Test]
         public async Task Can_login_user()
+        {
+            await GetInMemSessionOrLoginAsync();
+            _openTidlSession.LoginResult.SessionId.Should().NotBeEmpty();
+        }
+
+        [Test]
+        public async Task Can_get_user_playlists()
+        {
+            await GetInMemSessionOrLoginAsync();
+            var jsonList = await _openTidlSession.GetUserPlaylists();
+            Console.WriteLine($"Got {jsonList.Items.Length} of {jsonList.TotalNumberOfItems}");
+        }
+
+        [Test]
+        public async Task Can_get_fav_playlists()
+        {
+            await GetInMemSessionOrLoginAsync();
+            var jsonList = await _openTidlSession.GetFavoritePlaylists();
+            Console.WriteLine($"Got {jsonList.Items.Length} of {jsonList.TotalNumberOfItems}");
+        }
+
+        [Test]
+        public async Task Can_get_fav_tracks()
+        {
+            await GetInMemSessionOrLoginAsync();
+            var jsonList = await _openTidlSession.GetFavoriteTracks();
+            Console.WriteLine($"Got {jsonList.Items.Length} of {jsonList.TotalNumberOfItems}");
+        }
+
+        [Test]
+        public async Task Can_get_fav_artists()
+        {
+            await GetInMemSessionOrLoginAsync();
+            var jsonList = await _openTidlSession.GetFavoriteArtists();
+            Console.WriteLine($"Got {jsonList.Items.Length} of {jsonList.TotalNumberOfItems}");
+        }
+
+        private async Task GetInMemSessionOrLoginAsync()
+        {
+            _openTidlSession = _openTidlSession ?? await LoginUserAsync();
+        }
+
+        private static Task<OpenTidlSession> LoginUserAsync()
         {
             var appSettingsReader = new AppSettingsReader();
             var username = appSettingsReader.GetValue("tidal.username", typeof(string)) as string;
             var password = appSettingsReader.GetValue("tidal.password", typeof(string)) as string;
-            var session = await TidalIntegrator.LoginUserAsync(username, password);
-            session.LoginResult.SessionId.Should().NotBeEmpty();
+            return TidalIntegrator.LoginUserAsync(username, password);
         }
     }
 }
